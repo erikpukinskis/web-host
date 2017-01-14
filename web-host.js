0a3ac644f47dd97565d4b64a202544fb81c6fb3a
Erik Pukinskis <erik@snowedin.net>
2017-01-13 18:48:10 -0800

    A home page for hosting multiple apps on one server

---
+++ web-host.js
@@ -0,0 +1,56 @@
+var library = require("module-library")(require)
+
+module.exports = library.export(
+  "web-host",
+  ["browser-bridge", "web-site", "with-nearby-modules", "web-element"],
+  function(BrowserBridge, WebSite, withNearbyModules, element) {
+
+    var voxelTemplate = element.template.container(
+      ".voxel",
+      element.style({
+        "width": "440px",
+        "padding": "10px",
+        "border": "10px solid #eee",
+      })
+    )
+
+    function webHost(port) {
+
+      var host = new WebSite()
+
+      withNearbyModules.aModuleAppeared(
+        "web-site",
+        function() {
+          var appServer = new WebSite()
+          host.use(appServer.app)
+          return appServer
+        }
+      )
+
+      host.addRoute("get", "/",
+        function(request, response) {
+          bridge = new BrowserBridge()
+          bridge.addToHead(element.stylesheet(voxelTemplate))
+
+          var voxels = []
+          withNearbyModules.aModuleAppeared(
+            "browser-bridge",
+            function() {
+              var partial = bridge.partial()
+              voxels.push(voxelTemplate(partial))
+              return partial
+            }
+          )
+
+          bridge.requestHandler(voxels)(request, response)
+        }
+      )
+
+      host.start(port || process.env.PORT || 8183)
+    }
+
+    return webHost
+  }
+)
+
+
